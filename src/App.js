1e291ecd3544e8497b3379d1e646bd388fdc405a
Heba Tarek <53930699+hebatarek1989@users.noreply.github.com>
2022-06-18 21:48:46 +0200

    using props react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
+import Data from "./data/data"
+import Card from "./components/Card"
 
-function App() {
+function App()
+{
+  const cards = Data.map(item=>{
+    return (
+      <Card 
+        key={item.id}
+        {...item}
+      />
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function NavBar()
+{
+    return (
+        <nav className="nav">
+            <img alt="airbnb logo" src="../images/airbnb-logo.png" className="nav--logo"/>
+        </nav>
+    );
+}
+
+export default NavBar;
